b28e9983bb82f31c6fd2e41e75f2bd157729c9a9
Guilherme Pereira <guilospanck@protonmail.com>
2022-10-17 17:22:29 +0100

    Add `Three.js` and render a plan on the canvas

--- src/pages/Home/viewModels/homeViewModel.ts
+++ src/pages/Home/viewModels/homeViewModel.ts
@@ -1,7 +1,66 @@
-import { UseHomeViewModelReturnType } from "../types";
+import { useEffect, useRef } from 'react'
+import { UseHomeViewModelReturnType } from '../types'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
 export const useHomeViewModel = (): UseHomeViewModelReturnType => {
+  const rendererRef = useRef<THREE.WebGLRenderer>()
+  const sceneRef = useRef<THREE.Scene>()
+  const cameraRef = useRef<THREE.PerspectiveCamera>()
+
+  useEffect(() => {
+    rendererRef.current = new THREE.WebGLRenderer({ canvas: document.getElementById('webgl') })
+    rendererRef.current.setSize(window.innerWidth, window.innerHeight)
+
+    sceneRef.current = new THREE.Scene()
+    cameraRef.current = new THREE.PerspectiveCamera(
+      45,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    )
+
+    const orbit = new OrbitControls(cameraRef.current, rendererRef.current.domElement)
+
+    cameraRef.current.position.set(10, 15, -22)
+
+    orbit.update()
+
+    const planeMesh = new THREE.Mesh(
+      new THREE.PlaneGeometry(20, 20),
+      new THREE.MeshBasicMaterial({
+        side: THREE.DoubleSide,
+        visible: false
+      })
+    )
+    planeMesh.rotateX(-Math.PI / 2)
+
+    sceneRef.current.add(planeMesh)
+
+    const grid = new THREE.GridHelper(20, 20)
+
+    sceneRef.current.add(grid)
+
+    const highlightMesh = new THREE.Mesh(
+      new THREE.PlaneGeometry(1, 1),
+      new THREE.MeshBasicMaterial({
+        side: THREE.DoubleSide,
+        transparent: true
+      })
+    )
+    highlightMesh.rotateX(-Math.PI / 2)
+    highlightMesh.position.set(0.5, 0, 0.5)
+
+    sceneRef.current.add(highlightMesh)
+
+    rendererRef.current.setAnimationLoop(animate)
+  }, [])
+
+  const animate = () => {
+    rendererRef.current.render(sceneRef.current, cameraRef.current)
+  }
+
   return {
     val: 1
   }
-};
+}
--- src/pages/Home/views/index.tsx
+++ src/pages/Home/views/index.tsx
@@ -1,6 +1,8 @@
-import React from "react";
-import { HomeViewProps } from "../types";
+import React from 'react'
+import { HomeViewProps } from '../types'
 
 export const HomeView = ({ viewModel }: HomeViewProps) => {
-  return <h1 title="hello-title">Hello {viewModel.val}</h1>;
-};
+  // return <h1 title="hello-title">Hello {viewModel.val}</h1>;
+  console.log(viewModel.val)
+  return <canvas id="webgl" width={window.innerWidth} height={window.innerHeight}></canvas>
+}
