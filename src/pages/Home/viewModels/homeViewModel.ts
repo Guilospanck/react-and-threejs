384b397b3fb0dd1697d4d1ceb8c68642bef281ce
Guilherme Pereira <guilospanck@protonmail.com>
2022-10-18 14:31:54 +0100

    Add feature of mousemove to more than one plane

--- src/pages/Home/viewModels/homeViewModel.ts
+++ src/pages/Home/viewModels/homeViewModel.ts
@@ -3,28 +3,40 @@ import { UseHomeViewModelReturnType } from '../types'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
+type GridProps = {
+  width?: number, height?: number
+}
+type MeshProps = {
+  width?: number, height?: number, visible?: boolean, transparent?: boolean
+}
+
 export const useHomeViewModel = (): UseHomeViewModelReturnType => {
   const rendererRef = useRef<THREE.WebGLRenderer>()
   const sceneRef = useRef<THREE.Scene>()
   const cameraRef = useRef<THREE.PerspectiveCamera>()
-  const planeMeshRef = useRef<THREE.Mesh<THREE.PlaneGeometry, THREE.MeshBasicMaterial>>()
   const highlightMeshRef = useRef<THREE.Mesh<THREE.PlaneGeometry, THREE.MeshBasicMaterial>>()
   const raycasterRef = useRef<THREE.Raycaster>()
   const mousePositionRef = useRef<THREE.Vector2>()
   const intersectsRef = useRef<THREE.Intersection<THREE.Object3D<THREE.Event>>[]>([])
   const canvasWidthRef = useRef<number>()
   const canvasHeightRef = useRef<number>()
+  const FLOOR_HEIGHT: number = 4 // defines the height of each floor of our "building"
+  const GRID_LENGTH: number = 20
 
   useEffect(() => {
+    /** Get canvas object */
     const canvas = document.getElementById('webgl')
     canvasWidthRef.current = canvas.clientWidth
     canvasHeightRef.current = canvas.clientHeight
 
+    /** Create and set the size of a WebGL renderer */
     rendererRef.current = new THREE.WebGLRenderer({ canvas })
     rendererRef.current.setSize(canvasWidthRef.current, canvasHeightRef.current)
 
+    /** Create the scene object */
     sceneRef.current = new THREE.Scene()
 
+    /** Create a perspective camera with the aspect ratio of the Canvas */
     const aspectRatio = canvasWidthRef.current / canvasHeightRef.current
     cameraRef.current = new THREE.PerspectiveCamera(
       45,
@@ -33,37 +45,37 @@ export const useHomeViewModel = (): UseHomeViewModelReturnType => {
       1000
     )
 
+    /** Orbit allows us to rotate and translate our view */
     const orbit = new OrbitControls(cameraRef.current, rendererRef.current.domElement)
 
-    cameraRef.current.position.set(0, 35, 0)
+    /** Defines initial camera position */
+    cameraRef.current.position.set(20, 20, 20)
 
     orbit.update()
 
-    planeMeshRef.current = new THREE.Mesh(
-      new THREE.PlaneGeometry(20, 20),
-      new THREE.MeshBasicMaterial({
-        side: THREE.DoubleSide,
-        visible: false
-      })
-    )
-    planeMeshRef.current.rotateX(-Math.PI / 2)
-    sceneRef.current.add(planeMeshRef.current)
+    /** Creates plane meshes */
+    const planeMesh = meshBuilder({})
+    planeMesh.rotateX(-Math.PI / 2)
 
-    const grid = new THREE.GridHelper(20, 20)
-    const anotherGrid = new THREE.GridHelper(20, 20)
-    anotherGrid.translateY(2)
+    const anotherPlaneMesh = meshBuilder({})
+    anotherPlaneMesh.rotateX(-Math.PI / 2) // when rotating an object, you are also rotating its coordinate axes
+    anotherPlaneMesh.translateZ(FLOOR_HEIGHT) // this is why here we're using translateZ
 
+    /** Adds our plane meshes to our scene */
+    sceneRef.current.add(planeMesh)
+    sceneRef.current.add(anotherPlaneMesh)
+
+    /** Creates grid to help visualize our planes */
+    const grid = gridBuilder({})
+    const anotherGrid = gridBuilder({})
+    anotherGrid.translateY(FLOOR_HEIGHT)
+
+    /** Adds our grids to our scene */
     sceneRef.current.add(grid)
-    // sceneRef.current.add(anotherGrid);
+    sceneRef.current.add(anotherGrid)
 
     /** Add central Highlight Mesh */
-    highlightMeshRef.current = new THREE.Mesh(
-      new THREE.PlaneGeometry(1, 1),
-      new THREE.MeshBasicMaterial({
-        side: THREE.DoubleSide,
-        transparent: true
-      })
-    )
+    highlightMeshRef.current = meshBuilder({ width: 1, height: 1, transparent: true, visible: true })
     highlightMeshRef.current.rotateX(-Math.PI / 2)
     highlightMeshRef.current.position.set(0.5, 0, 0.5)
 
@@ -90,21 +102,58 @@ export const useHomeViewModel = (): UseHomeViewModelReturnType => {
 
       raycasterRef.current.setFromCamera(mousePositionRef.current, cameraRef.current)
 
-      intersectsRef.current = raycasterRef.current.intersectObject(planeMeshRef.current)
+      intersectsRef.current = raycasterRef.current.intersectObjects(sceneRef.current.children)
 
       if (intersectsRef.current.length === 0) {
         return
       }
 
-      const intersect = intersectsRef.current[0]
-      const highlightPos = new THREE.Vector3().copy(intersect.point).floor().addScalar(0.5)
-      highlightMeshRef.current.position.set(highlightPos.x, 0, highlightPos.z)
+      /** Get interceptions with Mesh objects */
+      let meshIntersects: THREE.Intersection<THREE.Object3D<THREE.Event>>[] = []
+      for (let i = 0; i < intersectsRef.current.length; i++) {
+        const object = intersectsRef.current[i].object
+        if (object.type !== 'Mesh') { continue }
+
+        const intersect = intersectsRef.current[i]
+        meshIntersects.push(intersect)
+        meshIntersects = meshIntersects.sort((a, b) => {
+          const aPositionZ = a.object.position.z
+          const bPositionZ = b.object.position.z
+
+          if (aPositionZ > bPositionZ) { return 1 }
+          if (aPositionZ < bPositionZ) { return -1 }
+          return 0
+        })
+      }
+
+      if (meshIntersects.length === 0) { return }
+
+      // Adds 0.5 because it will render the center of the square object (1x1), so we must offset by 0.5
+      // to render it properly inside the grid
+      const highlightPos = new THREE.Vector3().copy(meshIntersects[0].point).floor().addScalar(0.5)
+      const currentPosition = [highlightPos.x, meshIntersects[0].object.position.y, highlightPos.z]
+      highlightMeshRef.current.position.set(currentPosition[0], currentPosition[1], currentPosition[2])
     }
 
     window.addEventListener('mousemove', (e) => callback(e))
 
     return () => window.removeEventListener('mousemove', callback)
   }, [])
+
+  const gridBuilder = ({ width = GRID_LENGTH, height = GRID_LENGTH }: GridProps): THREE.GridHelper => {
+    return new THREE.GridHelper(width, height)
+  }
+
+  const meshBuilder = ({ width = GRID_LENGTH, height = GRID_LENGTH, visible = false, transparent = false }: MeshProps): THREE.Mesh<THREE.PlaneGeometry, THREE.MeshBasicMaterial> => {
+    return new THREE.Mesh(
+      new THREE.PlaneGeometry(width, height),
+      new THREE.MeshBasicMaterial({
+        side: THREE.DoubleSide,
+        visible,
+        transparent
+      })
+    )
+  }
 
   const animate = () => {
     rendererRef.current.render(sceneRef.current, cameraRef.current)
